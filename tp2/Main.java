a8ee280d56100de5b9a48a209847203b602966d0
FabricioXVII <fabricio1400000@gmail.com>
2025-09-16 20:26:12 +0000

    parcial 1

--- tp2/Main.java
+++ tp2/Main.java
@@ -1,26 +1,2 @@
 package tp2;
 
-public class Main {
-    public static void main(String[] args) {
-        Tele tv = new Tele("Samsung");
-
-        tv.encender();
-
-        tv.setCanal(5);
-        System.out.println("Canal actual: " + tv.getCanal());
-
-        tv.subirCanal();
-        tv.subirCanal();
-        System.out.println("Canal después de subir: " + tv.getCanal());
-
-        tv.volverAlCanalAnterior();
-        System.out.println("Canal después de volver: " + tv.getCanal());
-
-        tv.subirVolumen();
-        tv.subirVolumen();
-        tv.bajarVolumen();
-        System.out.println("Volumen actual: " + tv.getVolumen());
-
-        tv.apagar();
-    }
-}
